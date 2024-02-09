ed017640cbbb5c0a9d3a470c0e7f822bdeb1a8cf
LuigiQ02 <140318152+LuigiQ02@users.noreply.github.com>
2024-02-10 00:37:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function confirmAttendance() {
+    var name = prompt("Please write " + "Bali " + "below");
+    if (name) {
+        alert("Thank you, " + name + "! ILOVEYOUUUUU! " + "See you soon! ");
+    } else {
+        alert("Please enter your name");
+    }
+}
+
+function cantSayNo() {
+    var name = ("MAG NO PANGET AND WALANG FRIES");
+    if (name) {
+        alert("BAWAL MAG NO AND WALANG FRIES");
+    } else {
+        alert("Please enter your name");
+    }
+}
